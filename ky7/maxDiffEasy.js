1207fed55fbf9b4074523793012ede46afffe698
nikitenus <nikitenuss@mail.ru>
2024-11-07 08:56:41 +0300

    feat: add "max diff - easy" function

---
+++ ky7/maxDiffEasy.js
@@ -0,0 +1,23 @@
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// Have fun!You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// Have fun!
+
+function maxDiff(list) {
+    if (list.length <= 1) {
+      return 0
+    } else {
+      let arr = list.sort((a, b) => a - b)
+      return arr[arr.length - 1] - arr[0]}
+  }
