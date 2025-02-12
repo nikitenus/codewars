d2a575b9b577f47053cb6007cbab6da4ab969c05
nikitenus <nikitenuss@mail.ru>
2025-02-12 13:59:31 +0300

    feat: 12.02.25 (7ky)

---
+++ ky7/strongNumber.js
@@ -0,0 +1,29 @@
+// Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+
+// For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+// Task
+// Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+
+// Examples
+// 1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+// 123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+// 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+// 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+
+
+function strong(n) {
+	let arrN = n
+    .toString()
+		.split('')
+		.map(el => {
+			let numb = +el
+      function factorialRecursive(numb) {if (numb === 0 || numb === 1) {
+				return 1
+			}
+			return numb * factorialRecursive(numb - 1)}
+      return factorialRecursive(numb);
+		})
+		.reduce((el, acc) => el + acc, 0)
+    return arrN === n ? "STRONG!!!!" : "Not Strong !!"
+}
