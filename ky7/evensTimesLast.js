f05834a24005a4796f6d77215e967e6f014c5c7a
nikitenus <nikitenuss@mail.ru>
2025-03-11 11:32:13 +0300

    fix: evensTimesLast fix

--- ky7/evensTimesLast.js
+++ ky7/evensTimesLast.js
@@ -1,12 +1,12 @@
 // Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
 //
-//     Indices in sequence start from 0.
+// Indices in sequence start from 0.
 //
 // If the sequence is empty, you should return 0.
 //
 
 function evenLast(numbers) {
-    return numbers.filter(el => el % 2 === 0).reduce((a,b)=>a+b)
+    return numbers.length && numbers.filter((el, i) => i % 2 === 0).reduce((a,b)=>a+b) * numbers[numbers.length - 1]
 }
 
 console.log(evenLast([2, 3, 4, 5]))
