e4c32c5d77fdd50befcfb73d763f23eb760d5b9e
vlad <nikitenuss@mail.ru>
2026-04-07 14:43:25 +0300

    feat: 07.04 (7 ky)

---
+++ ky7/Largest Square Inside A Circle.js
@@ -0,0 +1,8 @@
+// Determine the area of the largest square that can fit inside a circle with radius r.
+
+function areaLargestSquare(r) {
+  return (r * 2) ** 2 / 2;
+}
+
+console.log(areaLargestSquare(5)); // 50
+console.log(areaLargestSquare(7)); // 98
