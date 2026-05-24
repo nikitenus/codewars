d87c652ea0792d45b87579c326d46c89a743cb90
nikiten <nikitenuss@mail.ru>
2026-05-24 13:32:06 +0300

    feat: 1st git task

---
+++ ky8/Opposite number.js
@@ -0,0 +1,15 @@
+// Very simple, given a number, find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return number * -1;
+}
+
+console.log(opposite(4.25));
+console.log(opposite(3.3333333));
+console.log(opposite(-12525220.3325));
